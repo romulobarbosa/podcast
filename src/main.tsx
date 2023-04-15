04011321ffd4049dc465f18041c2f9546fb6aa70
Rômulo Barbosa <barbosaromulo@gmail.com>
2023-04-15 15:13:58 +0200

    Create routes and error page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import ErrorPage from "./pages/error"
+import HomePage from "./pages/home"
+import PodcastPage from "./pages/podcast"
+
+import "./index.css"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/podcast/:id",
+    element: <PodcastPage />,
+  },
+])
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
---
+++ src/pages/podcast/index.tsx
@@ -0,0 +1,9 @@
+const PodcastPage = () => {
+  return (
+    <div id="podcast-page">
+      <p>Podcast internal page</p>
+    </div>
+  )
+}
+
+export default PodcastPage
