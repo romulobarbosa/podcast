522d39ccce1c56d375e6aad23101db24e2a000e8
Rômulo Barbosa <barbosaromulo@gmail.com>
2023-04-16 16:11:01 +0200

    test input component

--- src/ui/components/Input/input.test.tsx
+++ src/ui/components/Input/input.test.tsx
@@ -1,11 +1,43 @@
-import { render, screen } from "@testing-library/react"
+import "@testing-library/jest-dom"
+import { fireEvent, render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 
 import Input from "./"
 
+const onCLickFunction = jest.fn()
+
 describe("input", () => {
   it("should render correctly", () => {
-    render(<Input />)
+    render(
+      <Input
+        placeholder="placeholder test"
+        className="class-test"
+        onChange={onCLickFunction}
+      />
+    )
 
-    expect(screen.getByRole("textbox")).toBeInTheDocument
+    const input = screen.getByRole("textbox")
+    expect(input).toBeInTheDocument()
+    expect(input).toHaveClass("class-test")
+    expect(screen.getByPlaceholderText("placeholder test")).toBeInTheDocument()
+  })
+
+  it("should change value and call onChange function", () => {
+    render(
+      <Input
+        placeholder="placeholder test"
+        className="class-test"
+        onChange={onCLickFunction}
+      />
+    )
+
+    const input = screen.getByRole("textbox") as HTMLInputElement
+
+    fireEvent.change(input, { target: { value: "search value" } })
+    expect(input.value).toBe("search value")
+    expect(onCLickFunction).toBeCalledTimes(1)
+    fireEvent.change(input, { target: { value: "new search value" } })
+    expect(input.value).toBe("new search value")
+    expect(onCLickFunction).toBeCalledTimes(2)
   })
 })
